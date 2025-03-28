dc20ac86baf914f14fa4682f755192a269a48f65
Grillo <s.deleonmanuel@gmail.com>
2025-03-28 00:14:30 -0300

    Aprendiendo de variables en Python

---
+++ 02-variables.py
@@ -0,0 +1,33 @@
+# Las variables en python tienen la siguiente sintaxis:
+# nombre_variable = valor
+
+# Ejemplo:
+nombre = "Juan"
+edad = 25
+
+# Estas son mutables
+
+#las variables de nombres compuestos se escriben en camelCase o en snake_case
+nombre_completo = "Juan Perez"
+nombreCompleto = "Juan Perez"
+
+# Como dato: 
+# Python tiene la capacidad de anular variables usando el prefijo o palabra reservada "del"
+
+del nombre #La variable nombre ya no existe
+
+# NOTA📢: Los strings tienen algo llamado operadores de pertenencia, que son:
+# in y not in
+
+# Ejemplo:
+
+name = "Juan Perez"
+
+print("Juan" in name) #True
+print("Juan" not in name) #False
+
+
+
+
+
+
