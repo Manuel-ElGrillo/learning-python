8b57745297f92fdb0eca155834eb3794ba0a6f07
Grillo <s.deleonmanuel@gmail.com>
2025-03-30 22:53:07 -0300

    Operadores de comparación

---
+++ 05-operadores-de-comparacion.py
@@ -0,0 +1,10 @@
+# En Python existen los siguientes operadores de comparación:
+
+# == Igual
+# != Distinto
+# > Mayor que
+# < Menor que
+# >= Mayor o igual que
+# <= Menor o igual que
+
+# Cada uno de estos operadores devuelve un booleano (True o False)
