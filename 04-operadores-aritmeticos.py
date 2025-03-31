756816d9f19992ff98a956d0b24504ab044a2d67
Grillo <s.deleonmanuel@gmail.com>
2025-03-30 22:48:16 -0300

    Operadores aritméticos

---
+++ 04-operadores-aritmeticos.py
@@ -0,0 +1,40 @@
+# En Python existen los siguientes operadores aritméticos:
+
+# + Suma
+# - Resta
+# * Multiplicación
+# / División
+# % Módulo
+# ** Potencia
+# // División entera
+
+# Ejemplo de suma:
+suma = 5 + 3
+print("suma: ", suma)
+
+# Ejemplo de resta:
+resta = 5 - 3
+print("resta: ", resta)
+
+# Ejemplo de multiplicación:
+multiplicacion = 5 * 3
+print("multiplicacion: ", multiplicacion)
+
+# Ejemplo de división:
+division = 5 / 3
+print("division: ", division) 
+
+# Ejemplo de módulo:
+modulo = 5 % 3
+print("modulo: ", modulo)
+
+# Ejemplo de potencia:
+potencia = 5 ** 3
+print("potencia: ", potencia)
+
+# Ejemplo de división entera:
+# La división entera es aquella que devuelve el cociente de la división entre dos números.
+division_entera = 4 // 3
+print("division_entera: ", division_entera)
+
+#Un detalle con la división entera o división  baja es que si el número resultante es un float, lo redondea hacia abajo
