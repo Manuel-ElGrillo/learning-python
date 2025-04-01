933574c034caacf4fd56ab23182d4aa7d9d8ade7
Grillo <s.deleonmanuel@gmail.com>
2025-03-31 22:08:31 -0300

    Operadores lógicos

---
+++ 07-operadores-logicos.py
@@ -0,0 +1,22 @@
+# Los operadores logicos son: and, or, not
+
+#El operador and devuelve True si ambas expresiones son True
+
+#El operador or devuelve True si al menos una de las expresiones es True
+
+#El operador not devuelve True si la expresión es False
+
+#AND
+resultado_1 = True and True #True
+
+#OR
+resultado_2 = True or False #True
+
+#NOT
+resultado_3 = not True #False
+#Lo tricky del NOT es que si la expresión es True, devuelve False y viceversa
+
+print("resultado_1: ", resultado_1)
+print("resultado_2: ", resultado_2)
+print("resultado_3: ", resultado_3)
+
