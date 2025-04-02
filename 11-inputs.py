77fe31912a70cb305dada45b82901072c748c344
Grillo <s.deleonmanuel@gmail.com>
2025-04-02 00:01:46 -0300

    Inputs

---
+++ 11-inputs.py
@@ -0,0 +1,62 @@
+# Inputs
+
+# Los inputs son una función que se utiliza para obtener datos del usuario
+
+nombre = input("Ingrese su nombre: ")
+print("Jódete, " + nombre)
+
+# Si quisiera que el input sea un número, puedo usar la función int()
+
+numero = int(input("Ingrese un número: "))
+print("Chúpate esta 🥚 " + str(numero) + " veces")
+#Se rompe si uso otro tipo de dato
+
+#Funciona igual si quiero que sea un decimal
+
+numero_decimal = float(input("Ingrese un número decimal: "))
+print("Chúpate esta 🥚 " + str(numero_decimal) + " veces")
+#Se rompe si se usan comas (,) en lugar de puntos (.)
+
+# Si quisiera que el input sea una lista, puedo usar la función list()
+
+lista = list(input("Ingrese una lista: "))
+print("La lista ingresada es: " + str(lista))
+
+# Si quisiera que el input sea una tupla, puedo usar la función tuple()
+
+tupla = tuple(input("Ingrese una tupla: "))
+print("La tupla ingresada es: " + str(tupla))
+
+# Si quisiera que el input sea un diccionario, puedo usar la función dict()
+
+diccionario = dict(input("Ingrese un diccionario: "))
+print("El diccionario ingresado es: " + str(diccionario))
+
+# Si quisiera que el input sea un conjunto, puedo usar la función set()
+
+conjunto = set(input("Ingrese un conjunto: "))
+print("El conjunto ingresado es: " + str(conjunto))
+
+# Si quisiera que el input sea un booleano, puedo usar la función bool()
+
+booleano = bool(input("Ingrese un booleano: "))
+print("El booleano ingresado es: " + str(booleano))
+
+# Si quisiera que el input sea una cadena de caracteres, puedo usar la función str()
+
+cadena = str(input("Ingrese una cadena de caracteres: "))
+print("La cadena ingresada es: " + cadena)
+
+# Si quisiera que el input sea una función, puedo usar la función lambda()
+
+# funcion = lambda x: x + 1
+# print("La función ingresada es: " + str(funcion))
+#LOL esta no me la sabía ._.
+
+
+
+
+
+
+
+
