ad0e0af9c476aebfafeba4cd71a287fd154c4b26
Grillo <s.deleonmanuel@gmail.com>
2025-04-08 00:50:08 -0300

    While

---
+++ 16-Bucle-while.py
@@ -0,0 +1,17 @@
+# Los bucles while son bucles que se ejecutan mientras una condición sea verdadera.
+
+# Sintaxis:
+# while condición:
+#     bloque de código
+
+# Ejemplo:
+
+lista = ["Tocineta", "Papas", "Crema de leche", "Cebolla", "Pollo"]
+
+index = 0
+
+while index < len(lista):
+    print(lista[index])
+    index += 1
+
+
